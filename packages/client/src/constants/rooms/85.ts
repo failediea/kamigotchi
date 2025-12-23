78c44885f359afdbcb589a7fd24882ca74fa8096
Tisiphone <93127339+Xenofluxx@users.noreply.github.com>
2025-12-23 15:28:05 +0100

    Add coop to skeleton hand (#2264)

--- packages/client/src/constants/rooms/85.ts
+++ packages/client/src/constants/rooms/85.ts
@@ -1,3 +1,4 @@
+import { triggerGoalModal } from 'app/triggers';
 import { bgPlaytestDay } from 'assets/images/rooms/85_giants-palm';
 import { giantsPalm } from 'assets/sound/ost';
 import { Room } from './types';
@@ -9,5 +10,11 @@ export const room85: Room = {
     key: 'giantsPalm',
     path: giantsPalm,
   },
-  objects: [],
+  objects: [
+    {
+      name: 'skeletal hand',
+      coordinates: { x1: 72, y1: 80, x2: 136, y2: 130 },
+      onClick: () => triggerGoalModal([7]),
+    },
+  ],
 };
