f8bb7dfbb8688436b610898ae870048ee39c22b7
Tisiphone <93127339+Xenofluxx@users.noreply.github.com>
2026-01-28 11:19:58 +0100

    Add new Items 26-01-2026 (#2323)

--- packages/client/src/assets/images/items/index.ts
+++ packages/client/src/assets/images/items/index.ts
@@ -1,7 +1,13 @@
 import aetheric_sextant from './aetheric_sextant.png';
 import agency_giftbox from './agency_giftbox.png';
+import ancient_automata from './ancient_automata.png';
+import ancient_ledger from './ancient_ledger.png';
 import ancient_machine_part from './ancient_machine_part.png';
+import ancient_tape from './ancient_tape.png';
 import animistic_poison from './animistic_poison.png';
+import antique_automata from './antique_automata.png';
+import antique_ledger from './antique_ledger.png';
+import antique_tape from './antique_tape.png';
 import apology_letter from './apology_letter.png';
 import ash_pearl from './ash_pearl.png';
 import ashlar from './ashlar.png';
@@ -19,6 +25,9 @@ import chalkberry from './chalkberry.png';
 import cheeseburger from './cheeseburger.png';
 import cigarette_butt from './cigarette_butt.png';
 import cleaning_fluid from './cleaning_fluid.png';
+import crawling_cope from './crawling_cope.png';
+import crawling_greed from './crawling_greed.png';
+import crawling_wrath from './crawling_wrath.png';
 import cthonic_blight from './cthonic_blight.png';
 import cultivation_i_spell_card from './cultivation_i_spell_card.png';
 import cultivation_ii_spell_card from './cultivation_ii_spell_card.png';
@@ -30,6 +39,9 @@ import disc_shaped_map from './disc_shaped_map.png';
 import djed_pillar from './djed_pillar.png';
 import dowsing_rod from './dowsing_rod.png';
 import dried_stems from './dried_stems.png';
+import elder_critter from './elder_critter.png';
+import elder_gumdrop from './elder_gumdrop.png';
+import elder_leafling from './elder_leafling.png';
 import empty_cup from './empty_cup.png';
 import energy_drink from './energy_drink.png';
 import essence_of_daffodil from './essence_of_daffodil.png';
@@ -56,6 +68,9 @@ import grace_potion from './grace_potion.png';
 import great_heart_crystal from './great_heart_crystal.png';
 import greater_xp_potion from './greater_xp_potion.png';
 import half_heart_crystal from './half_heart_crystal.png';
+import heirloom_automata from './heirloom_automata.png';
+import heirloom_ledger from './heirloom_ledger.png';
+import heirloom_tape from './heirloom_tape.png';
 import holy_dust from './holy_dust.png';
 import holy_syrup from './holy_syrup.png';
 import honeydew_scale from './honeydew_scale.png';
@@ -84,6 +99,9 @@ import kamigotchi_world_passport_yellow from './kamigotchi_world_passport_yellow
 import kw_maps_data_chip from './kw_maps_data_chip.png';
 import mana_mochi from './mana_mochi.png';
 import maple_flavor_ghost_gum from './maple_flavor_ghost_gum.png';
+import mask_of_avarice from './mask_of_avarice.png';
+import mask_of_contempt from './mask_of_contempt.png';
+import mask_of_mischief from './mask_of_mischief.png';
 import melkarths_heroic_awakening_spell_card from './melkarths_heroic_awakening_spell_card.png';
 import microplastics from './microplastics.png';
 import mint from './mint.png';
@@ -93,6 +111,9 @@ import musu from './musu.png';
 import musu_magnet from './musu_magnet.png';
 import neiths_river_of_life_spell_card from './neiths_river_of_life_spell_card.png';
 import obol from './obol.png';
+import old_critter from './old_critter.png';
+import old_gumdrop from './old_gumdrop.png';
+import old_leafling from './old_leafling.png';
 import onyx_shard from './onyx_shard.webp';
 import otherworld_coin from './otherworld_coin.png';
 import paeons_field_of_flowers_spell_card from './paeons_field_of_flowers_spell_card.png';
@@ -125,13 +146,28 @@ import spice_grinder from './spice_grinder.png';
 import spirit_glue from './spirit_glue.png';
 import stone from './stone.png';
 import sunset_apple_mochi from './sunset_apple_mochi.png';
+import swarming_cope from './swarming_cope.png';
+import swarming_greed from './swarming_greed.png';
+import swarming_wrath from './swarming_wrath.png';
 import switchbox_imp from './switchbox_imp.png';
 import teardrop_jewel from './teardrop_jewel.png';
+import teeming_cope from './teeming_cope.png';
+import teeming_greed from './teeming_greed.png';
+import teeming_wrath from './teeming_wrath.png';
 import timber from './timber.png';
 import toadstool_liquor from './toadstool_liquor.png';
 import uninteresting_paste from './uninteresting_paste.png';
 import unmarked_data_chip from './unmarked_data_chip.png';
+import veil_of_avarice from './veil_of_avarice.png';
+import veil_of_contempt from './veil_of_contempt.png';
+import veil_of_mischief from './veil_of_mischief.png';
 import vipp from './vipp.png';
+import visage_of_avarice from './visage_of_avarice.png';
+import visage_of_contempt from './visage_of_contempt.png';
+import visage_of_mischief from './visage_of_mischief.png';
+import wise_critter from './wise_critter.png';
+import wise_gumdrop from './wise_gumdrop.png';
+import wise_leafling from './wise_leafling.png';
 import wonder_egg from './wonder_egg.png';
 import wooden_stick from './wooden_stick.png';
 import xp_candy_huge from './xp_candy_huge.png';
@@ -143,11 +179,17 @@ import xp_potion from './xp_potion.png';
 export const ItemImages = {
   aetheric_sextant: aetheric_sextant,
   agency_giftbox: agency_giftbox,
+  ancient_automata: ancient_automata,
+  ancient_ledger: ancient_ledger,
   ancient_machine_part: ancient_machine_part,
-  animistic_poison: animistic_poison, // INSERT HERE
+  ancient_tape: ancient_tape,
+  animistic_poison: animistic_poison,
+  antique_automata: antique_automata,
+  antique_ledger: antique_ledger,
+  antique_tape: antique_tape,
   apology_letter: apology_letter,
   ash_pearl: ash_pearl,
-  ashlar: ashlar, // INSERT HERE
+  ashlar: ashlar,
   astrolabe_disk: astrolabe_disk,
   berry_chalk: berry_chalk,
   best_ice_cream: best_ice_cream,
@@ -155,38 +197,44 @@ export const ItemImages = {
   black_poppy: black_poppy,
   black_poppy_extract: black_poppy_extract,
   bless_potion: bless_potion,
-  blue_pansy: blue_pansy, // INSERT HERE
-  bone_chunk: bone_chunk, // INSERT HERE
+  blue_pansy: blue_pansy,
+  bone_chunk: bone_chunk,
   booster_pack: booster_pack,
   chalkberry: chalkberry,
   cheeseburger: cheeseburger,
-  cigarette_butt: cigarette_butt, // INSERT HERE
-  cleaning_fluid: cleaning_fluid, // INSERT HERE
-  cthonic_blight: cthonic_blight, // INSERT HERE
+  cigarette_butt: cigarette_butt,
+  cleaning_fluid: cleaning_fluid,
+  crawling_cope: crawling_cope,
+  crawling_greed: crawling_greed,
+  crawling_wrath: crawling_wrath,
+  cthonic_blight: cthonic_blight,
   cultivation_i_spell_card: cultivation_i_spell_card,
   cultivation_ii_spell_card: cultivation_ii_spell_card,
   cultivation_iii_spell_card: cultivation_iii_spell_card,
   cultivation_iv_spell_card: cultivation_iv_spell_card,
-  curse_tablet: curse_tablet, // INSERT HERE
+  curse_tablet: curse_tablet,
   daffodil: daffodil,
   disc_shaped_map: disc_shaped_map,
-  djed_pillar: djed_pillar, // INSERT HERE
+  djed_pillar: djed_pillar,
   dowsing_rod: dowsing_rod,
-  dried_stems: dried_stems, // INSERT HERE
+  dried_stems: dried_stems,
+  elder_critter: elder_critter,
+  elder_gumdrop: elder_gumdrop,
+  elder_leafling: elder_leafling,
   empty_cup: empty_cup,
   energy_drink: energy_drink,
   essence_of_daffodil: essence_of_daffodil,
-  essence_of_hearing: essence_of_hearing, // INSERT HERE
-  essence_of_sight: essence_of_sight, // INSERT HERE
-  essence_of_smell: essence_of_smell, // INSERT HERE
-  essence_of_taste: essence_of_taste, // INSERT HERE
-  essence_of_thought: essence_of_thought, // INSERT HERE
-  essence_of_touch: essence_of_touch, // INSERT HERE
-  fast_travel_potion: fast_travel_potion, // INSERT HERE
+  essence_of_hearing: essence_of_hearing,
+  essence_of_sight: essence_of_sight,
+  essence_of_smell: essence_of_smell,
+  essence_of_taste: essence_of_taste,
+  essence_of_thought: essence_of_thought,
+  essence_of_touch: essence_of_touch,
+  fast_travel_potion: fast_travel_potion,
   festival_chime: festival_chime,
   fetid_egg: fetid_egg,
-  flash_talisman: flash_talisman, // INSERT HERE
-  fortified_xp_potion: fortified_xp_potion, // INSERT HERE
+  flash_talisman: flash_talisman,
+  fortified_xp_potion: fortified_xp_potion,
   fuliginous_ooze: fuliginous_ooze,
   full_heart_crystal: full_heart_crystal,
   gacha_ticket: gacha_ticket,
@@ -194,17 +242,20 @@ export const ItemImages = {
   gaokerena_mochi: gaokerena_mochi,
   gingerbread_cookie: gingerbread_cookie,
   glass_jar: glass_jar,
-  golden_apple: golden_apple, // INSERT HERE
+  golden_apple: golden_apple,
   grace_potion: grace_potion,
   great_heart_crystal: great_heart_crystal,
   greater_xp_potion: greater_xp_potion,
   half_heart_crystal: half_heart_crystal,
+  heirloom_automata: heirloom_automata,
+  heirloom_ledger: heirloom_ledger,
+  heirloom_tape: heirloom_tape,
   holy_dust: holy_dust,
   holy_syrup: holy_syrup,
-  honeydew_scale: honeydew_scale, // INSERT HERE
+  honeydew_scale: honeydew_scale,
   hostility_potion: hostility_potion,
   ice_cream: ice_cream,
-  ingot: ingot, // INSERT HERE
+  ingot: ingot,
   inverted_teardrop_jewel: inverted_teardrop_jewel,
   irradiated_root: irradiated_root,
   kami_mochi: kami_mochi,
@@ -227,6 +278,9 @@ export const ItemImages = {
   kw_maps_data_chip: kw_maps_data_chip,
   mana_mochi: mana_mochi,
   maple_flavor_ghost_gum: maple_flavor_ghost_gum,
+  mask_of_avarice: mask_of_avarice,
+  mask_of_contempt: mask_of_contempt,
+  mask_of_mischief: mask_of_mischief,
   melkarths_heroic_awakening_spell_card: melkarths_heroic_awakening_spell_card,
   microplastics: microplastics,
   mint: mint,
@@ -236,19 +290,22 @@ export const ItemImages = {
   musu_magnet: musu_magnet,
   neiths_river_of_life_spell_card: neiths_river_of_life_spell_card,
   obol: obol,
+  old_critter: old_critter,
+  old_gumdrop: old_gumdrop,
+  old_leafling: old_leafling,
   onyx_shard: onyx_shard,
-  otherworld_coin: otherworld_coin, // INSERT HERE
+  otherworld_coin: otherworld_coin,
   paeons_field_of_flowers_spell_card: paeons_field_of_flowers_spell_card,
   pale_potion: pale_potion,
   pallid_root_extract: pallid_root_extract,
-  patinated_pipe: patinated_pipe, // INSERT HERE
+  patinated_pipe: patinated_pipe,
   pine_cone: pine_cone,
   pine_pollen: pine_pollen,
   plastic_bottle: plastic_bottle,
   pom_pom_fruit_candy: pom_pom_fruit_candy,
   portable_burner: portable_burner,
   powdered_red_amber: powdered_red_amber,
-  pure_essence: pure_essence, // INSERT HERE
+  pure_essence: pure_essence,
   pyramid_engine: pyramid_engine,
   red_amber_crystal: red_amber_crystal,
   red_ribbon_gummy: red_ribbon_gummy,
@@ -256,11 +313,11 @@ export const ItemImages = {
   resin: resin,
   resin_tincture: resin_tincture,
   respec_potion: respec_potion,
-  rock_candyfloss: rock_candyfloss, // INSERT HERE
-  rock_crystal_lens: rock_crystal_lens, // INSERT HERE
+  rock_candyfloss: rock_candyfloss,
+  rock_crystal_lens: rock_crystal_lens,
   sanguine_shroom: sanguine_shroom,
   sanguineous_powder: sanguineous_powder,
-  sarcophagus_honey: sarcophagus_honey, // INSERT HERE
+  sarcophagus_honey: sarcophagus_honey,
   scrap_metal: scrap_metal,
   screwdriver: screwdriver,
   shredded_mint: shredded_mint,
@@ -268,13 +325,28 @@ export const ItemImages = {
   spirit_glue: spirit_glue,
   stone: stone,
   sunset_apple_mochi: sunset_apple_mochi,
-  switchbox_imp: switchbox_imp, // INSERT HERE
+  swarming_cope: swarming_cope,
+  swarming_greed: swarming_greed,
+  swarming_wrath: swarming_wrath,
+  switchbox_imp: switchbox_imp,
   teardrop_jewel: teardrop_jewel,
-  timber: timber, // INSERT HERE
-  toadstool_liquor: toadstool_liquor, // INSERT HERE
+  teeming_cope: teeming_cope,
+  teeming_greed: teeming_greed,
+  teeming_wrath: teeming_wrath,
+  timber: timber,
+  toadstool_liquor: toadstool_liquor,
   uninteresting_paste: uninteresting_paste,
-  unmarked_data_chip: unmarked_data_chip, // INSERT HERE
+  unmarked_data_chip: unmarked_data_chip,
+  veil_of_avarice: veil_of_avarice,
+  veil_of_contempt: veil_of_contempt,
+  veil_of_mischief: veil_of_mischief,
   vipp: vipp,
+  visage_of_avarice: visage_of_avarice,
+  visage_of_contempt: visage_of_contempt,
+  visage_of_mischief: visage_of_mischief,
+  wise_critter: wise_critter,
+  wise_gumdrop: wise_gumdrop,
+  wise_leafling: wise_leafling,
   wonder_egg: wonder_egg,
   wooden_stick: wooden_stick,
   xp_candy_huge: xp_candy_huge,
